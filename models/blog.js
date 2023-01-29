71cd1fcac84f8931135bc5800f5608b3aad0c3cf
fiosman <fares.osman@outlook.com>
2023-01-29 17:36:40 -0500

    adds blogs table

---
+++ models/blog.js
@@ -0,0 +1,38 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Blog extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Blog.init(
+    {
+      author: {
+        type: DataTypes.STRING,
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      likes: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Blog",
+    }
+  );
+  return Blog;
+};
