d263fca3b19aee263b83bbf729729307431ca96b
fiosman <fares.osman@outlook.com>
2023-02-02 23:57:14 -0500

    adds more stuff

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const port = 3002;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
