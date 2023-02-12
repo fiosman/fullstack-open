464e7b41c6b8717cd9d919b6f96303f6ba804293
fiosman <fares.osman@outlook.com>
2023-02-11 23:48:26 -0500

    adds initial logic for controller actions

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -7,9 +7,23 @@ const getAllBlogs = async (req, res, next) => {
   }
 };
 
-const deleteBlog = async (req, res, next) => {};
+const deleteBlog = async (req, res, next) => {
+  try {
+    await BlogService.deleteBlog(req.params.id);
+    res.json({ success: "ok" });
+  } catch (e) {
+    throw e;
+  }
+};
 
-const createBlog = async (req, res, next) => {};
+const createBlog = async (req, res, next) => {
+  try {
+    const createdBlog = await BlogService.createBlog(req.body);
+    res.json(createdBlog);
+  } catch (e) {
+    throw e;
+  }
+};
 
 module.exports = {
   getAllBlogs,
