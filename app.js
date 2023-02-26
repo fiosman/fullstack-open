fcb0cb0515519248f287e4ab2713e5529878aa0d
fiosman <fares.osman@outlook.com>
2023-02-25 22:03:19 -0500

    begins to add userRouter

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const blogRouter = require("./routes/blog-routes");
+const userRouter = require("./routes/user-routes");
 const app = express();
 const port = 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/blogs", blogRouter);
+app.use("/api/users", userRouter);
 
 app.listen(port, () => {
   console.log(`Server started and listening on port ${port}`);
