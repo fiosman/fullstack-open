6a64534ed6109fc18726f419cd85d254bcafba77
fiosman <fares.osman@outlook.com>
2023-04-03 23:15:09 -0400

    adds logic to create a user blog

--- services/ReadingListService.js
+++ services/ReadingListService.js
@@ -1,3 +1,12 @@
-const addBlogToReadingList = async (blogId, userId) => {};
+const UserBlog = require("../models").UserBlog;
+
+const addBlogToReadingList = async (blogId, userId) => {
+  try {
+    const userBlog = await UserBlog.create({ userId, blogId });
+    return userBlog;
+  } catch (err) {
+    throw err;
+  }
+};
 
 module.exports = { addBlogToReadingList };
